74631043ee36b3cdaeb9198f097c496f5fa259f4
killje <P.A.Beuks@student.rug.nl>
2018-09-19 23:06:46 +0200

    Four square service provider rebased4 (#12)
    * Clean Laravel installation (5.7)
    
    * Added the foursquare service provider
    
    * On start of application, checks ContentProvider status
    * In case API is offline, write error
    * Everything configurable in .env
    * OO-design implementing interfaces
    * ContentProvider accessible from Laravel's Service Container
    * Created Service Provider
    
    * Venue listing by using the API
    
    * Metadata and gitignore
    
    * Resolve conflict
    
    * updated gitignore for .idea files
    
    * Remove /api/ prefix for endpoints
    
    * Clean Laravel installation (5.7) (#3)
    
    * Clean Laravel installation (5.7)
    
    * Updated index.php for production/development diff
    
    Added a fix for url differences in production and development

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -16,3 +16,5 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/venues/lookupbycoords', 'API\VenuesController@lookUpByCoords')->name('venues.lookupbycoords');
