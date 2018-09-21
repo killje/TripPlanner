69b21de84a0eabad8c2e76405e5c9eccd6f8b5a7
killje <P.A.Beuks@student.rug.nl>
2018-09-21 19:26:13 +0200

    Added routing to the front-end (#19)
    Added routing to the fornt end

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,26 +1,41 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 import {AngularOpenlayersModule} from 'ngx-openlayers';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import {RouterModule, Routes} from '@angular/router';
 
-import { AppComponent } from './app.component';
-import { MapComponent } from './map/map.component';
-import { MapViewComponent } from './map/map-view/map-view.component';
-import { MapListComponent } from './map/map-list/map-list.component';
+import {AppComponent} from './app.component';
+import {MapComponent} from './map/map.component';
+import {MapViewComponent} from './map/map-view/map-view.component';
+import {MapListComponent} from './map/map-list/map-list.component';
+
+const appRoutes: Routes = [
+    {path: 'map', component: MapComponent},
+    {
+        path: '',
+        redirectTo: '/map',
+        pathMatch: 'full'
+    },
+    {path: '**', component: MapComponent}
+];
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    MapComponent,
-    MapViewComponent,
-    MapListComponent
-  ],
-  imports: [
-    BrowserModule,
-    NgbModule,
-    AngularOpenlayersModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        MapComponent,
+        MapViewComponent,
+        MapListComponent
+    ],
+    imports: [
+        RouterModule.forRoot(
+            appRoutes,
+            {enableTracing: true} // <-- debugging purposes only
+        ),
+        BrowserModule,
+        NgbModule,
+        AngularOpenlayersModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
