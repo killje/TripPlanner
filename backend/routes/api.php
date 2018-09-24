5de9b6d9bc232fbce4995d2fb713df64c3590faa
ProducerMark <SoelmanM@gmail.com>
2018-09-24 18:20:12 +0200

    Updated routing and readme

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -21,7 +21,7 @@ Route::get('/venues/lookupbycoords', 'API\VenuesController@lookUpByCoords')->nam
 Route::get('/venues/details', 'API\VenuesController@getDetails')->name('venues.details');
 
 Route::post('/trips/store', 'API\TripController@store')->name('trips.store');
-Route::post('/trips/destroy', 'API\TripController@destroy')->name('trips.destroy');
+Route::delete('/trips/destroy', 'API\TripController@destroy')->name('trips.destroy');
 Route::post('/trips/venues/add', 'API\TripController@addVenue')->name('trips.addvenue');
-Route::post('/trips/venues/remove', 'API\TripController@removeVenue')->name('trips.removevenue');
-Route::post('/trips/venues/list', 'API\TripController@listVenues')->name('trips.listvenues');
+Route::delete('/trips/venues/remove', 'API\TripController@removeVenue')->name('trips.removevenue');
+Route::get('/trips/venues/list', 'API\TripController@listVenues')->name('trips.listvenues');
