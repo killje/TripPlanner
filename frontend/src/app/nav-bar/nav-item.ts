2b34b4ebf562a73437cf454b188ebf4402473ce8
killje <killje@github.com>
2018-10-06 12:55:47 +0200

    Frontpage uses now the same style as trips

--- frontend/src/app/nav-bar/nav-item.ts
+++ frontend/src/app/nav-bar/nav-item.ts
@@ -4,10 +4,12 @@ export class NavItem {
     private name: string;
     private link: string;
     private active: boolean = true;
+    private matchStrict: boolean;
 
-    public constructor(name: string, link: string) {
+    public constructor(name: string, link: string, matchStrict: boolean = false) {
         this.name = name;
         this.link = link;
+        this.matchStrict = matchStrict;
     }
 
     public getName(): string {
@@ -25,4 +27,8 @@ export class NavItem {
     public setActive(active: boolean): void {
         this.active = active;
     };
+    
+    public isMatchStrict(): boolean {
+        return this.matchStrict;
+    }
 }
