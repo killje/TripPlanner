2fe6cf693a2c2340b7fbc3282e0c4ed4d8ff0e0f
killje <killje@github.com>
2018-10-10 22:28:25 +0200

    DragAndDrop polyfill

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -11,3 +11,18 @@ if (environment.production) {
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.log(err));
 
+import {polyfill} from 'mobile-drag-drop';
+// optional import of scroll behaviour
+import {scrollBehaviourDragImageTranslateOverride} from "mobile-drag-drop/scroll-behaviour";
+
+polyfill({
+    // use this to make use of the scroll behaviour
+    dragImageTranslateOverride: scrollBehaviourDragImageTranslateOverride
+});
+
+// workaround to make scroll prevent work in iOS Safari > 10
+try {
+    window.addEventListener("touchmove", function () {}, {passive: false});
+}
+catch (e) {}
+
